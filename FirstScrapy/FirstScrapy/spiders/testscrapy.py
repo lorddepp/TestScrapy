9a6d626df6799f9c896f6b86a1ce9b173c266858
lorddepp <lorddepp@163.com>
2020-05-19 21:42:53 +0800

    addtest

--- FirstScrapy/FirstScrapy/spiders/testscrapy.py
+++ FirstScrapy/FirstScrapy/spiders/testscrapy.py
@@ -2,7 +2,7 @@
 import scrapy
 from FirstScrapy.items import FirstscrapyItem
 from selenium import webdriver
-
+# aaaaaaa
 class TestscrapySpider(scrapy.Spider):
     name = 'testscrapy'
     # allowed_domains = ['www.xxx.com']
